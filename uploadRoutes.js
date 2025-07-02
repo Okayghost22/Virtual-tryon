44c7efab8db45d06eafb2f1f139825e943b969f1
Okayghost22 <sj5873@srmist.edu.in>
2025-07-02 23:05:09 +0530

    Create uploadRoutes.js

---
+++ uploadRoutes.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const express = require("express");
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  res.json({ path: `/uploads/${req.file.filename}` });
+});
+
+module.exports = router;
